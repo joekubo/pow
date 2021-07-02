03f4bedd8151610336353c1edac75f27af345373
joe <joe@localhost.localdomain>
2021-07-02 14:32:07 +0300

    first commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,282 @@
+@extends("mainlayout")
+@section("title","HOME")
+@section('content')
+      <section id="home" class="video-hero js-fullheight" style="height: 700px; background-image: url(images/life.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+  		  <div class="overlay js-fullheight"></div>
+  			<!-- <a class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=AKolQceAZQk',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, quality:'default'}"></a> -->
+  		  <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-10 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+                  <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">That they may <strong>not perish</strong></h1>
+                  <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="https://www.youtube.com/watch?v=3jYhPVRpL7c" class="btn btn-primary btn-outline-white px-4 py-3 popup-vimeo">You want to be Born Again?</a></p>
+              </div>
+            </div>
+        </div>
+  	</section>
+
+		<section class="ftco-bible-study">
+			<div class="container-wrap">
+				<div class="col-md-12 wrap">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12 d-md-flex">
+								<div class="ftco-animate">
+									<h3>JOHN 1</h3>
+                  <p>&nbsp;  In the beginning was the <strong>WORD</strong>, and the <strong>WORD</strong> was with God, and the <strong>WORD</strong> was God.  He was with God in the beginning.  Through him all things were made; without him nothing was made that has been made.  In him was life, and that life was the light of all mankind. 5 The light shines in the darkness, and the darkness has not overcome it.  There was a man sent from God whose name was John. He came as a witness to testify concerning that light, so that through him all might believe. He himself was not the light; he came only as a witness to the light.
+                  </p>
+								</div>
+								<!-- <div class="one-half d-md-flex align-items-md-center ftco-animate">
+									<div class="countdown-wrap">
+										<p class="countdown d-flex">
+											<span id="days"></span>
+											<span id="hours"></span>
+											<span id="minutes"></span>
+											<span id="seconds"></span>
+										</p>
+									</div>
+									<div class="button">
+										<p><a href="#" class="btn btn-primary p-3">Events Details</a></p>
+									</div>
+								</div> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ftco-section-2">
+      <div class="container-fluid">
+        <div class="section-2-blocks-wrapper d-flex row no-gutters">
+          <div class="img col-md-6 ftco-animate" style="background-image: url('images/1.jpg');">
+            <a href="https://www.youtube.com/watch?v=J5V2Nr1gQOQ" class="button popup-vimeo"><span class="ion-ios-play"></span></a>
+          </div>
+          <div class="text col-md-6 ftco-animate">
+            <div class="text-inner align-self-start">
+
+              <h3>Daniel 11:32</h3>
+              <p>
+                And such as do wickedly against the covenant shall he corrupt by flatteries: but the people that do know their God shall be strong, and do exploits.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-5">
+          <div class="col-md-6 text-center heading-section ftco-animate">
+            <span class="subheading">Our Services</span>
+            <h2 class="mb-4">Giving light to someone</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in</p>
+          </div>
+        </div>
+        <div class="row" style="padding: 15%;">
+          <div class="col-sm-12 col-md-6 col-lg-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon d-flex justify-content-center mb-3"><span class="align-self-center fa fa-pencil-square"></span></div></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">I'm New Here</h3>
+                <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="https://www.youtube.com/watch?v=3jYhPVRpL7c" class="btn btn-primary btn-outline-blue px-4 py-3 popup-vimeo">Click to register</a></p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-sm-12 col-md-6 col-lg-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon d-flex justify-content-center mb-3"><span class="align-self-center flaticon-pray"></span></div></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Pray requests</h3>
+                <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="https://www.youtube.com/watch?v=3jYhPVRpL7c" class="btn btn-primary btn-outline-blue px-4 py-3 popup-vimeo">Click to Request</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-12 col-md-6 col-lg-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon d-flex justify-content-center mb-3"><span class="align-self-center flaticon-cross"></span></div></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Our Services</h3>
+                <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="https://www.youtube.com/watch?v=3jYhPVRpL7c" class="btn btn-primary btn-outline-blue px-4 py-3 popup-vimeo">View Services</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="ftco-section-parallax">
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-12 text-center heading-section heading-section-white ftco-animate">
+              <h2>Our Branches</h2>
+              <hr>
+              <div class="row">
+                <div class="col-md-4 text-left heading-section heading-section-white ftco-animate">
+                  <p><a href="#" style="color:yellow;">1. Hunimgum, UK</a></p>
+                  <p><a href="#" style="color:yellow;">2. Johanesburg, SA</a></p>
+                  <p><a href="#" style="color:yellow;">3. Tudor, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">4. Nyali, Mombasa</a></p>
+                </div>
+                <div class="col-md-4 text-left heading-section heading-section-white ftco-animate">
+                  <p><a href="#" style="color:yellow;">5. Bokole, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">6. Mtwapa, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">7. Mkomani, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">8. Chaani, Mombasa</a></p>
+                </div>
+                <div class="col-md-4 text-left heading-section heading-section-white ftco-animate">
+                  <p><a href="#" style="color:yellow;">9. Konkodia, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">10. Shanzu, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">11. Kiembeni, Mombasa</a></p>
+                  <p><a href="#" style="color:yellow;">12. Kongowea, Mombasa</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row no-gutters justify-content-center mb-5 pb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+            <span class="subheading">Sermons</span>
+            <h2 class="mb-4">Watch our sermons</h2>
+            <p>So then faith comes by hearing, and hearing by the word of God.</p>
+          </div>
+        </div>
+        <div class="row">
+        	<div class="col-md-4 ftco-animate">
+        		<div class="sermons">
+        			<a href="https://www.youtube.com/watch?v=J5V2Nr1gQOQ" class="img popup-vimeo mb-3 d-flex justify-content-center align-items-center" style="background-image: url(images/sermon1.jpeg);">
+        				<div class="icon d-flex justify-content-center align-items-center">
+        					<span class="icon-play"></span>
+        				</div>
+        			</a>
+        			<div class="text">
+        				<h3><a href="#">Trust in the Lord</a></h3>
+        				<span class="position">Pastor Osborne Juma </span>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-md-4 ftco-animate">
+        		<div class="sermons">
+        			<a href="https://www.youtube.com/watch?v=g8TMcyY12OI" class="img popup-vimeo mb-3 d-flex justify-content-center align-items-center" style="background-image: url(images/sermon2.jpeg);">
+        				<div class="icon d-flex justify-content-center align-items-center">
+        					<span class="icon-play"></span>
+        				</div>
+        			</a>
+        			<div class="text">
+        				<h3><a href="#">Easter</a></h3>
+        				<span class="position">Pastor Osborne Juma</span>
+        			</div>
+        		</div>
+        	</div>
+        	<div class="col-md-4 ftco-animate">
+        		<div class="sermons">
+        			<a href="https://www.youtube.com/watch?v=FTc9c_3h8PE&t=520s" class="img popup-vimeo mb-3 d-flex justify-content-center align-items-center" style="background-image: url(images/sermons-3.jpg);">
+        				<div class="icon d-flex justify-content-center align-items-center">
+        					<span class="icon-play"></span>
+        				</div>
+        			</a>
+        			<div class="text">
+        				<h3><a href="#">Prayers, Presence, and Provision</a></h3>
+        				<span class="position">Dave Zuleger</span>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+        <div class="row mt-5">
+        	<div class="col text-center">
+        		<p><a href="#" class="btn btn-primary btn-outline-primary p-3">Watch all sermons</a></p>
+        	</div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+            <span class="subheading">Read, Get Inspired with the testimonies of individuals</span>
+            <h2 class="mb-4">Testimonies</h2>
+            <p><strong>1 Chronicles 16:8: </strong> Give praise to the Lord, proclaim his name; make known among the nations what he has done.</p>
+          </div>
+        </div>
+        <div class="row ftco-animate">
+          <div class="col-md-12">
+            <div class="carousel-testimony owl-carousel ftco-owl">
+              <div class="item text-center">
+                <div class="testimony-wrap p-4 pb-5">
+                  <div class="user-img mb-4" style="background-image: url(images/daddy.jpeg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                      <i class="icon-quote-left"></i>
+                    </span>
+                  </div>
+                  <div class="text">
+                    <p class="mb-5">The Lord instantly healed a man who was brought in with an ambulance</p>
+                    <p class="name">Bishop Elisha Juma</p>
+                    <span class="position">Bishop</span>
+                    <a href="https://www.youtube.com/watch?v=SKdXnY1Yyaw" class="img popup-vimeo mb-3 d-flex justify-content-center align-items-center">
+                      <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="icon-play"></span>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- <section class="ftco-section-2 bg-light">
+    	<div class="container-fluid">
+    		<div class="row no-gutters d-flex">
+    			<div class="col-md-6 img d-flex justify-content-end align-items-center" style="background-image: url(images/event.jpg);">
+    				<div class="col-md-7 heading-section text-sm-center text-md-right heading-section-white ftco-animate mr-md-5 mt-md-5">
+	            <h2>Our latest events</h2>
+	            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in</p>
+	            <p><a href="#" class="btn btn-primary py-3 px-4">View Events</a></p>
+	          </div>
+    			</div>
+    			<div class="col-md-6">
+    				<div class="event-wrap">
+	    				<div class="event-entry d-flex ftco-animate">
+	    					<div class="meta mr-4">
+	    						<p>
+	    							<span>07</span>
+	    							<span>Aug 2018</span>
+	    						</p>
+	    					</div>
+	    					<div class="text">
+	    						<h3 class="mb-2"><a href="events.html">Saturday's Bible Reading</a></h3>
+	    						<p class="mb-4"><span>9:00am at 456 NC USA</span></p>
+	    						<a href="events.html" class="img" style="background-image: url(images/event-1.jpg);"></a>
+	    					</div>
+	    				</div>
+	    				<div class="event-entry d-flex ftco-animate">
+	    					<div class="meta mr-4">
+	    						<p>
+	    							<span>07</span>
+	    							<span>Aug 2018</span>
+	    						</p>
+	    					</div>
+	    					<div class="text">
+	    						<h3 class="mb-2"><a href="events.html">Wednesday Gospel Singing</a></h3>
+	    						<p class="mb-4"><span>9:00am at 456 NC USA</span></p>
+	    						<a href="events.html" class="img" style="background-image: url(images/event-2.jpg);"></a>
+	    					</div>
+	    				</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section> -->
+@stop
---
+++ resources/views/mainlayout.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Power Of The Word Internation || @yield('title')</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="{{ URL::asset('https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800')}}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/animate.css')}}">
+    
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/magnific-popup.css')}}">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/aos.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/ionicons.min.css')}}">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css')}}">
+
+  </head>
+  <body>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.php"><i class="flaticon-cross"></i> <span>POWER OF THE WORD</span> <span>International</span></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.php" id="nav-link-home" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="#" data-toggle="modal" data-target="#mymodal" id="nav-link-about" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="#" data-toggle="modal" data-target="#mymodal" id="nav-link-events" class="nav-link">Events</a></li>
+	          <li class="nav-item"><a href="#" data-toggle="modal" data-target="#mymodal" id="nav-link-sermons" class="nav-link">Sermons</a></li>
+	          <li class="nav-item"><a href="#" data-toggle="modal" data-target="#mymodal"id="nav-link-contact" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    <!-- Body -->
+
+    @yield('content')
+    <!-- End Body -->
+
+   
+
+<!-- footer -->
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="logo"><i class="flaticon-cross"><a href="index.html"></i><span>POWER OF THE WORD</span><span>INTERNATIONAL</span></a></h2>
+              <p>&nbsp;  In the beginning was the <strong>WORD</strong>, and the <strong>WORD</strong> was with God, and the <strong>WORD</strong> was God.  He was with God in the beginning.  Through him all things were made; without him nothing was made that has been made.  In him was life, and that life was the light of all mankind...
+                  </p>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Quick Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Church kids</a></li>
+                <li><a href="#" class="py-2 d-block">Community</a></li>
+                <li><a href="#" class="py-2 d-block">Ministries</a></li>
+                <li><a href="#" class="py-2 d-block">Message</a></li>
+                <li><a href="#" class="py-2 d-block">Sermons</a></li>
+                <li><a href="#" class="py-2 d-block">Events</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Contact Information</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">198 West 21th Street, Suite 721 New York NY 10016</a></li>
+                <li><a href="#" class="py-2 d-block">+ 1235 2355 98</a></li>
+                <li><a href="#" class="py-2 d-block">info@yoursite.com</a></li>
+                <li><a href="#" class="py-2 d-block">email@email.com</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-clock-o"></span><span class="text">Saturday &mdash; Sunday 8:00am - 5:00pm</span></li>
+	              </ul>
+	            </div>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> Powered by <a href="https://tolclin.com" target="_blank">TOLCLIN</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="modal fade" id="mymodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Alert</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>This section is under development</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="{{ URL::asset('js/jquery.min.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{ URL::asset('js/popper.min.js')}}"></script>
+  <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.easing.1.3.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.waypoints.min.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.stellar.min.js')}}"></script>
+  <script src="{{ URL::asset('js/owl.carousel.min.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{ URL::asset('js/aos.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.animateNumber.min.js')}}"></script>
+  <script src="{{ URL::asset('js/bootstrap-datepicker.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.timepicker.min.js')}}"></script>
+  <script src="{{ URL::asset('js/jquery.mb.YTPlayer.min.js')}}"></script>
+  <script src="{{ URL::asset('js/scrollax.min.js')}}"></script>
+  <script src="{{ URL::asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false')}}"></script>
+  <script src="{{ URL::asset('js/google-map.js')}}"></script>
+  <script src="{{ URL::asset('js/main.js')}}"></script>
+  <script>
+    
+    $("body").delegate("#nav-link-about","click",function(){
+        $(".modal").show();
+    });
+    $("body").delegate("#nav-link-events","click",function(){
+        $(".modal").show();
+    });
+    $("body").delegate("#nav-link-sermons","click",function(){
+        $(".modal").show();
+    });
+    $("body").delegate("#nav-link-blog","click",function(){
+        $(".modal").show();
+    });
+
+    $("body").delegate("#nav-link-contact","click",function(){
+        $(".modal").show();
+    });
+
+
+  </script>
+
+  </body>
+</html>
